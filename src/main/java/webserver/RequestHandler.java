d3d52b2108373e27eb135a4f6c647e76914c4e30
jangdongho <ehdgh25@naver.com>
2023-07-11 11:47:15 +0900

    docs: README 작성

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,6 +10,7 @@ import webserver.utils.HttpUtil;
 import webserver.utils.view.FileUtil;
 
 public class RequestHandler implements Runnable {
+
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
@@ -30,6 +31,7 @@ public class RequestHandler implements Runnable {
             DataOutputStream dos = new DataOutputStream(out);
             response200Header(dos, body.length);
             responseBody(dos, body);
+
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
--- src/main/java/webserver/utils/HttpUtil.java
+++ src/main/java/webserver/utils/HttpUtil.java
@@ -3,10 +3,12 @@ package webserver.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import webserver.RequestHandler;
+import webserver.exception.InvalidRequestException;
 
 import java.io.*;
 
 public class HttpUtil {
+
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     public static String getContent(InputStream in) throws IOException {
@@ -22,7 +24,14 @@ public class HttpUtil {
 
             logger.debug(line);
         }
+
+        verifyContent(content);
+
         return content.toString();
+    }
+
+    private static void verifyContent(StringBuilder content) {
+        if(content.toString().isEmpty()) throw InvalidRequestException.Exception;
     }
 
     public static String getUrl(String content) throws IOException {
