f029fc83136bb4a3acd39fa7b774436c7323a581
jangdongho <ehdgh25@naver.com>
2023-07-12 10:17:05 +0900

    feat: UserController 로 회원가입 처리

--- src/main/java/model/user/factory/UserFactory.java
+++ src/main/java/model/user/factory/UserFactory.java
@@ -1,8 +1,8 @@
-package model.factory;
+package model.user.factory;
 
 import java.util.Map;
 
-import model.User;
+import model.user.User;
 
 public class UserFactory {
 
@@ -11,7 +11,7 @@ public class UserFactory {
 	private static final String NAME = "name";
 	private static final String EMAIL = "email";
 
-	public static final User createUser(Map<String, String> map) {
+	public static User createUser(Map<String, String> map) {
 		return new User(map.get(USER_ID), map.get(PASSWORD), map.get(NAME), map.get(EMAIL));
 	}
 }
---
+++ src/main/java/webserver/controller/UserController.java
@@ -0,0 +1,22 @@
+package webserver.controller;
+
+import java.util.Map;
+
+import container.Controller;
+import container.annotation.MyMapping;
+import db.Database;
+import model.user.User;
+import model.user.factory.UserFactory;
+
+@MyMapping("/user/create")
+public class UserController implements Controller {
+
+	@Override
+	public void execute(Map<String, String> model) {
+		User user = UserFactory.createUser(model);
+
+		Database.addUser(user);
+
+		System.out.println(Database.findUserById(user.getUserId()));
+	}
+}
